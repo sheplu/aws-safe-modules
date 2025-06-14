fc58fb457a1c26ba57c210d26ca9364713b6fb09
Jean Burellier <jean.burellier@gmail.com>
2025-06-13 22:48:51 -0400

    feat: add lambda validation for runtime and memorySize

--- base/Lambda.ts
+++ base/Lambda.ts
@@ -1,11 +1,31 @@
 import { LambdaFunction as aws_LambdaFunction, LambdaFunctionConfig } from '@cdktf/provider-aws/lib/lambda-function';
 import { Construct } from 'constructs';
+import { Ajv } from "ajv";
 
 export class LambdaFunction extends aws_LambdaFunction {
   constructor(scope: Construct, id: string, config: LambdaFunctionConfig) {
     const checkedConfig = { ...config };
     checkedConfig.functionName = `${config?.functionName}-lbd`;
 
+    const valid = validate(checkedConfig)
+    if (!valid) {
+      throw validate.errors
+    }
     super(scope, `asm/${id}`, checkedConfig);
   }
 };
+
+
+const ajv = new Ajv({ allErrors: true });
+
+const schema = {
+  type: "object",
+  properties: {
+    memorySize: { type: "number", minimum: 128, multipleOf: 64 },
+    runtime: { enum: ["nodejs22.x", "python3.13", "java21", "ruby3.4", "provided.al2023"] },
+  },
+  required: ["memorySize", "runtime"],
+  additionalProperties: true,
+}
+
+export const validate = ajv.compile(schema)
