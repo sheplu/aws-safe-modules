1f80e71a236d3d525887bb3119f058ebb4ad5f3f
Jean Burellier <jean.burellier@gmail.com>
2025-06-19 12:45:36 +0200

    feat: add rds validation

--- base/RDS.ts
+++ base/RDS.ts
@@ -1,5 +1,6 @@
 import { DbInstance as aws_DbInstance, DbInstanceConfig } from '@cdktf/provider-aws/lib/db-instance';
 import { Construct } from 'constructs';
+import { Ajv } from "ajv";
 
 export class DbInstance extends aws_DbInstance {
     constructor(scope: Construct, id: string, config: DbInstanceConfig) {
@@ -9,3 +10,19 @@ export class DbInstance extends aws_DbInstance {
         super(scope, `asm/${id}`, checkedConfig);
     }
 };
+
+const ajv = new Ajv({ allErrors: true });
+
+const schema = {
+  type: "object",
+  properties: {
+    storageType: { enum: ["gp3", "io1", "io2"] },
+    engine: { enum: ["postgres"] },
+    allocatedStorage: { type: "number", minimum: 30 },
+    engineVersion: { enum: ["17.4", "17.5"] },
+  },
+  required: ["storageType", "engine", "allocatedStorage"],
+  additionalProperties: true,
+}
+
+export const validate = ajv.compile(schema)
