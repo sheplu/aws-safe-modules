f7e3d399c8c6c39c9924b90c7434d7b499932e27
Jean Burellier <jean.burellier@gmail.com>
2025-07-01 00:19:17 +0200

    test: add base testing

---
+++ __tests__/modules/base.ts
@@ -0,0 +1,39 @@
+import "cdktf/lib/testing/adapters/jest"; 
+import { readdirSync } from "node:fs";
+
+describe("Modules Base", () => {
+  describe("Count number of modules", () => {
+    let folderList: Array<String>;
+
+    beforeAll(() => {
+      folderList = readdirSync('./base')
+    });
+
+    it("should should have 15 files", () => {
+      expect(folderList.length).toEqual(15);
+    });
+
+    it("should should have the exact files", () => {
+      const files: Array<String> = [
+        'AWSProvider.ts',
+        'ApiGateway.ts',
+        'CloudWatch.ts',
+        'Cloudfront.ts',
+        'Dynamodb.ts',
+        'ECR.ts',
+        'IAM.ts',
+        'Lambda.ts',
+        'RDS.ts',
+        'Route53.ts',
+        'S3Bucket.ts',
+        'SQS.ts',
+        'SSM.ts',
+        'VPC.ts',
+        'WAFv2.ts'
+      ];
+      
+      expect(folderList).toEqual(files);
+    });
+  });
+
+});
---
+++ __tests__/modules/managed-modules.ts
@@ -0,0 +1,26 @@
+import "cdktf/lib/testing/adapters/jest"; 
+import { readdirSync } from "node:fs";
+
+describe("Modules Base", () => {
+  describe("Count number of modules", () => {
+    let folderList: Array<String>;
+
+    beforeAll(() => {
+      folderList = readdirSync('./managed')
+    });
+
+    it("should should have 2 files", () => {
+      expect(folderList.length).toEqual(2);
+    });
+
+    it("should should have the exact files", () => {
+      const files: Array<String> = [
+        'S3Bucket.ts',
+        'WAFv2.ts',
+      ];
+      
+      expect(folderList).toEqual(files);
+    });
+  });
+
+});
