86713997196085d141bd1b184feee5deac5f5c11
Jean Burellier <jean.burellier@gmail.com>
2025-08-01 08:39:20 +0200

    feat: add data export for cur v2

--- base/Cost.ts
+++ base/Cost.ts
@@ -1,8 +1,18 @@
+import { BcmdataexportsExport as aws_BcmdataexportsExport, BcmdataexportsExportConfig } from '@cdktf/provider-aws/lib/bcmdataexports-export';
 import { CurReportDefinition as aws_CurReportDefinition, CurReportDefinitionConfig } from '@cdktf/provider-aws/lib/cur-report-definition';
 import { Construct } from 'constructs';
 
 export class CurReportDefinition extends aws_CurReportDefinition {
     constructor(scope: Construct, id: string, config: CurReportDefinitionConfig) {
+        const checkedConfig = { ...config };
+        checkedConfig.reportName = `${config?.reportName}-cur`;
+
+        super(scope, `asm/${id}`, checkedConfig);
+    }
+};
+
+export class BcmdataexportsExport extends aws_BcmdataexportsExport {
+    constructor(scope: Construct, id: string, config: BcmdataexportsExportConfig) {
         const checkedConfig = { ...config };
 
         super(scope, `asm/${id}`, checkedConfig);
